592d387f94007772bb16b01cbbba1e550a30ffaf
Madhur Wadhwa <madhurw7official@gmail.com>
2018-03-30 00:08:39 +0530

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA_e-s_W-D1Yikw4z5fPCGxXPxqxvoGN2g",
+    authDomain: "ang-shop-26dd7.firebaseapp.com",
+    databaseURL: "https://ang-shop-26dd7.firebaseio.com",
+    projectId: "ang-shop-26dd7",
+    storageBucket: "",
+    messagingSenderId: "432913246821"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA_e-s_W-D1Yikw4z5fPCGxXPxqxvoGN2g",
+    authDomain: "ang-shop-26dd7.firebaseapp.com",
+    databaseURL: "https://ang-shop-26dd7.firebaseio.com",
+    projectId: "ang-shop-26dd7",
+    storageBucket: "",
+    messagingSenderId: "432913246821"
+  }
 };
