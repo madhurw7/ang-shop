61c7e028b3b5736eba3f7b2169f00912fb057847
Madhur Wadhwa <madhurw7official@gmail.com>
2018-04-03 04:33:20 +0530

    Added route guards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './user.service';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
@@ -8,10 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private auth: AuthService, private router: Router){
+  constructor(private auth: AuthService, private router: Router, private userService: UserService){
     //This user$ will emitt every time a user logs in or logs out, when the user logs in, it will have a user object and the if condition will be true, that time it will redirect from anywhere that the user came from. If he logs out the if condition is false and the user stays on the same page.
     auth.user$.subscribe(user => {
       if (user) {
+        userService.save(user)
+
         let returnUrl = localStorage.getItem('returnUrl');
         router.navigateByUrl(returnUrl);
       }
